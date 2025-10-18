16ffc542170292a943c18e33a433f1391b865248
Nguyen Truong Hoang Anh <66248562+vn-anhnth@users.noreply.github.com>
2025-10-18 09:25:30 +0700

    Add condition to log model only if it's the best

--- ppocr/utils/loggers/wandb_logger.py
+++ ppocr/utils/loggers/wandb_logger.py
@@ -68,6 +68,9 @@ class WandbLogger(BaseLogger):
         self.run.log(updated_metrics, step=step)
 
     def log_model(self, is_best, prefix, metadata=None):
+        if not is_best:
+            return
+
         model_path = os.path.join(self.save_dir, prefix + ".pdparams")
         artifact = self.wandb.Artifact(
             "model-{}".format(self.run.id), type="model", metadata=metadata
